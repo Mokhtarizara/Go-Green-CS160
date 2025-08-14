758be1d7cfc6ca2132c1af3485ee2ad6bfda62cd
Zara <Gitzara@protonmail.com>
2025-08-14 14:49:19 -0700

    updating packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // change from BrowserRouter to HashRouter
+import { HashRouter } from "react-router-dom"; 
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter> 
       <App />
     </HashRouter>
   </React.StrictMode>
